72c1c47e8a1a2d69e4da688f35867b4e0b65c770
ignormies <bryce.beagle@gmail.com>
2021-03-31 14:39:32 -0700

    Rename ShortcakeClient to ShortcakeApi

--- test/client_test.dart
+++ test/client_test.dart
@@ -3,7 +3,7 @@ import 'package:test/test.dart';
 
 void main() {
   group('ShortcakeClient.introspect()', () {
-    var client = ShortcakeClient('http://localhost:8000');
+    var client = ShortcakeApi('http://localhost:8000');
 
     test('does not create an exception', () async {
       var result = await client.introspect();
